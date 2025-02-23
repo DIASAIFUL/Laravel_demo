b8e5c80bf170e4e208aea99ef401c89b78f4e1b6
MD Saiful Islam <saifulislam109327@gmai.com>
2025-02-23 15:09:54 +0600

    Route parameter

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 }) ->name('about');
+Route::get('/service', function () {
+    return view('service');
+}) ->name('service');
 
 Route::get('/service/{service_id}/{service_name}/{service_title?}', function ($service_id, $service_name,$service_title = null) {
     return "service".''.$service_id.''.$service_name.''.$service_title; 
@@ -18,6 +21,12 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
    
+
+Route::get('/user/{id}/{name}',function($id,$name){
+    echo' '.$id.' '.$name.'';
+
+})-> where( [ 'id' => '[0-9]+','name' => '[A-Za-z]+']);
+
 
 
 
