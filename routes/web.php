43d510d2b80b5b1297c0212a160c586428cfd128
MD Saiful Islam <saifulislam109327@gmai.com>
2025-02-23 11:16:56 +0600

    route& view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('about');
+});
+
+Route::get('/', function () {
+    return view('service');
+});
+
+Route::get('/', function () {
+    return view('contact');
+});
+
+
+
